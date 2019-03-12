a7e5b2fefa9c844bf6ea26fcafe298d76536509d
guinex <sachavha@syr.edu>
2019-03-11 23:00:46 -0400

    technologies added

--- config/initializers/autoload_constant.rb
+++ config/initializers/autoload_constant.rb
@@ -1,3 +1,9 @@
+TECHNOLOGIES = if (skills = SystemConstant.where(name: 'technologies', autoload: true).first)
+			skills.value.split(',')
+		else
+			[]
+		end
+
 SKILLS = if (skills = SystemConstant.where(name: 'skills', autoload: true).first)
 			skills.value.split(',')
 		else
