a94d0e22b0ce0add84697166a39e163dbfc5eb9b
guinex <sachavha@syr.edu>
2020-03-17 03:10:31 -0400

    new updates changes in courses and position

--- config/initializers/autoload_constant.rb
+++ config/initializers/autoload_constant.rb
@@ -9,3 +9,17 @@ SKILLS = if (skills = SystemConstant.where(name: 'skills', autoload: true).first
 		else
 			[]
 		end
+COURSES = if (skills = SystemConstant.where(name: 'courses', autoload: true).first)
+			skills.value.split(',')
+		else
+			[]
+		end
+# SystemConstant.where(name: 'courses', autoload: true).first_or_create(value: "Operating System,
+# Artificial neural Networks,
+# Analytical Data Mining,
+# Evolutionary Machine Learning,
+# Internet Security,
+# Social Media and Data Mining,
+# Design and Analysis of Algorithm,
+# Structural Programming and Formal Methods,
+# Database Management System")
