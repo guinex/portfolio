36d50eac02a029c0c90ddb376c750fc6b120700f
guinex <sachavha@syr.edu>
2020-12-22 00:21:15 -0500

    system cont added

--- config/initializers/autoload_constant.rb
+++ config/initializers/autoload_constant.rb
@@ -1,22 +1,22 @@
-# TECHNOLOGIES = if (skills = SystemConstant.where(name: 'technologies', autoload: true).first)
-# 			skills.value.split(',')
-# 		else
-# 			[]
-# 		end
+TECHNOLOGIES = if (skills = SystemConstant.where(name: 'technologies', autoload: true).first)
+			skills.value.split(',')
+		else
+			[]
+		end
 
-# SKILLS = if (skills = SystemConstant.where(name: 'skills', autoload: true).first)
-# 			skills.value.split(',')
-# 		else
-# 			[]
-# 		end
-# COURSES = if (skills = SystemConstant.where(name: 'courses', autoload: true).first)
-# 			skills.value.split(',')
-# 		else
-# 			[]
-# 		end
-# PROJECT_DETAIL_PATH = if File.exist?(Rails.root.join('data', 'projects.yml')) 
-# 		File.join(Rails.root.join('data', 'projects.yml'))
-# 	else
-# 		""
-# 	end	
+SKILLS = if (skills = SystemConstant.where(name: 'skills', autoload: true).first)
+			skills.value.split(',')
+		else
+			[]
+		end
+COURSES = if (skills = SystemConstant.where(name: 'courses', autoload: true).first)
+			skills.value.split(',')
+		else
+			[]
+		end
+PROJECT_DETAIL_PATH = if File.exist?(Rails.root.join('data', 'projects.yml')) 
+		File.join(Rails.root.join('data', 'projects.yml'))
+	else
+		""
+	end	
 # SystemConstant.where(name: 'courses', autoload: true).first_or_create(value: "Operating System,Artificial neural Networks,Analytical Data Mining,Evolutionary Machine Learning,Internet Security,Social Media and Data Mining,Design and Analysis of Algorithm,Structural Programming and Formal Methods,Database Management System")
